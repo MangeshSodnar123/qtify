6331fdfb79934fbf65e0e131890aa7ef2127e807
Mangesh Sodnar <mangeshsodnar123@gmail.com>
2023-10-06 16:31:46 +0530

    hero-qtify-takehome

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-
+import Hero from './components/Hero/Hero';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Hero />
     </>
   );
 }
